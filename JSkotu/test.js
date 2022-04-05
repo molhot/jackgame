d5137dccf95fdfd7f4b2020554e3098c791dbceb
molhot <sakata19991214@gmail.com>
2022-04-06 08:14:14 +0900

    first

---
+++ JSkotu/test.js
@@ -0,0 +1,38 @@
+//全カードの管理
+let cards = []
+//何回カードをめくるを推したか、確認
+let element
+
+//カードの用意
+for(let i = 0;i < 52;i = i + 1){
+    cards.push(i);
+}
+
+//カードの山をシャッフルする関数
+function shuffle(){
+    for(let i = 0;i <=20;i = i + 1){
+
+    }
+}
+//12で割った章が0ならスペード、といった感じで管理
+
+//1 画面を読み込んだ瞬間の関数
+window.addEventListener("load", #);
+
+//2 カードを引くボタンをクリックした際の関数(ボタンクリック⇒シャッフル⇒画面状にめくった画像の表示)
+element = document.querySelectorAll("#--")
+element.addEventListener("click", #)
+
+//勝負するボタンをクリックした際の関数(出てる画像の値を計算、結果の表示)
+element = document.querySelectorAll("#--")
+element.addEventListener("click", #)
+
+//もう一回遊ぶボタンをクリックした際の関数（全部元に戻す⇒表示）
+element = document.querySelectorAll("#--")
+element.addEventListener("click", #)
+
+//終了ボタンを押した場合
+element = document.querySelectorAll("#--")
+element.addEventListener("click", #)
+
+
